9403eddc98f355e2331ca3393dea496387129ed0
91818 <91818@LAPTOP-VIRJTL3L>
2023-07-21 19:38:59 +0530

    another new facebook launch class added

---
+++ LaunchProgram/src/test/java/TestClass/NewTest.java
@@ -0,0 +1,15 @@
+package TestClass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f()
+  {
+	  WebDriver driver=new ChromeDriver();
+	  driver.get("https://www.facebook.com/");
+
+  }
+}
